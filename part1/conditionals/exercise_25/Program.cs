a0de57706e0e7380842d1bdb34ca145940b48129
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-01-24 16:13:47 +0200

    exercises24-29

--- part1/conditionals/exercise_25/Program.cs
+++ part1/conditionals/exercise_25/Program.cs
@@ -7,6 +7,22 @@ namespace exercise_25
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number:");
+      string num = Console.ReadLine();
+      int intValue = Convert.ToInt32(num);
+
+      if (intValue == 1984)
+
+      {
+        Console.WriteLine("Orwell");
+      }
+
+      
+
+
+      
+      
+
 
     }
   }
--- part1/conditionals/exercise_27/Program.cs
+++ part1/conditionals/exercise_27/Program.cs
@@ -7,6 +7,24 @@ namespace exercise_27
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number:");
+      string num = Console.ReadLine();
+      int intValue = Convert.ToInt32(num);
+
+      if (intValue > 0)
+
+      {
+        Console.WriteLine("It is positive");
+      }
+
+      if (intValue <= 0)
+
+      {
+        Console.WriteLine("It is not positive");
+      }
+
+
+
 
     }
   }
