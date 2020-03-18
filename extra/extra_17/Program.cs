288fe2b65e01ced9b682c31f3af9b363804ba74e
JoakimBjorklind <joakim.bjorklind@centria.fi>
2020-03-18 12:02:09 +0200

    xtra17

--- extra/extra_17/Program.cs
+++ extra/extra_17/Program.cs
@@ -1,13 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_17
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Add your code here:
+        public static void Main(string[] args)
+        {
+            // Add your code here:
+            List<Item> items = new List<Item>();
 
+           
+            while (true)
+            {
+                Console.WriteLine("Identifier? (empty will stop):");
+                string identifier = (Console.ReadLine());
+                if (identifier == "")
+                {
+                    break;
+                }
+
+
+                Console.WriteLine("Name? (empty will stop):");
+                string name = Console.ReadLine();
+
+
+                if (name == "")
+                {
+                    break;
+                }
+                Item item = new Item(identifier, name);
+                if (items.Contains(item))
+                {
+                    continue;
+                }
+                else
+                {
+                    items.Add(item);
+                }
+
+            }
+
+            
+            Console.WriteLine("==Items==");
+            foreach (Item item in items)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
     }
-  }
 }
