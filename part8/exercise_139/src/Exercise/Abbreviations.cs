30aeff4243080dca65299a4e81e8661beca6c8f3
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-04-04 20:20:25 +0300

    ex.139

--- part8/exercise_139/src/Exercise/Abbreviations.cs
+++ part8/exercise_139/src/Exercise/Abbreviations.cs
@@ -3,21 +3,36 @@ using System.Collections.Generic;
 namespace Exercise
 {
   public class Abbreviations
-  {
+  { 
+    private Dictionary<string, string> abbreviations;
+    
+    public Abbreviations()
+    {
+       this.abbreviations = new Dictionary<string, string>();
+    }
+    
 
     public void AddAbbreviation(string abbreviation, string explanation)
     {
-
+      this.abbreviations.Add(abbreviation, explanation);
     }
 
     public bool HasAbbreviation(string abbreviation)
     {
+      if (this.abbreviations.ContainsKey(abbreviation))
+      {
+        return true;
+      }
       return false;
     }
 
     public string FindExplanationFor(string abbreviation)
     {
-      return null;
+      if (HasAbbreviation(abbreviation))
+      {
+        return this.abbreviations[abbreviation];
+      }
+      return "not found";
     }
   }
 }
