071a42483d45d07637d572a4d1fd39915a933ef7
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-04-11 22:49:25 +0300

    ex.149

--- part9/exercise_149/src/Exercise/People/Teacher.cs
+++ part9/exercise_149/src/Exercise/People/Teacher.cs
@@ -1,6 +1,18 @@
 namespace Exercise
 {
-  public class Teacher
-  {
-  }
+    using System;
+    public class Teacher : Person
+    {
+        public int salary { get; set; }
+
+        public Teacher(string name, string address, int salary) : base(name, address)
+        {
+            this.salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " salary " + this.salary + " per month";
+        }
+    }
 }
