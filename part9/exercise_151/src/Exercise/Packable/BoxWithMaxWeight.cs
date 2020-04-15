d311153e42f980e7b29ff2500b5e75a1eea01d01
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-04-15 17:50:59 +0300

    ex.154 and parts of 151.

--- part9/exercise_151/src/Exercise/Packable/BoxWithMaxWeight.cs
+++ part9/exercise_151/src/Exercise/Packable/BoxWithMaxWeight.cs
@@ -3,8 +3,10 @@ namespace Exercise
   using System.Collections.Generic;
   public class BoxWithMaxWeight : Box
   {
+    private int capacity;
     public BoxWithMaxWeight(int capacity)
     {
+      this.capacity = capacity;
     }
 
     public override void Add(Item item)
--- part9/exercise_154/src/Exercise/Herd/Herd.cs
+++ part9/exercise_154/src/Exercise/Herd/Herd.cs
@@ -1,24 +1,40 @@
 namespace Exercise
 {
   using System.Collections.Generic;
+  using System.Text;
   public class Herd : IMovable
   {
+    private List<IMovable> objects;
 
     public Herd()
     {
+      this.objects = new List<IMovable>();
     }
 
     public void AddToHerd(IMovable m)
     {
+      this.objects.Add(m);
     }
 
     public void Move(int dx, int dy)
     {
+      foreach (IMovable member in objects)
+      {
+        member.Move(dx, dy);
+      }
     }
 
     public override string ToString()
     {
-      return "";
+      // found this solution from stackoverflow. had to add the using.System.Text for it to get
+      // it to work with the StringBuilder. Not exactly sure how this thing works but it printed 
+      // out the right way with dotnet run and it passed the dotnet test.
+      StringBuilder strBuilder = new StringBuilder();
+      foreach (IMovable member in objects)
+      {
+         strBuilder.AppendLine(member.ToString());
+      }
+      return strBuilder.ToString();
     }
   }
 }
