a55af265176f958f2aaa7c3f96ff2b0f8bcb1dbc
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-04-26 21:01:35 +0300

    ex.165 4/5 test passed, 1 failed

--- part11/exercise_165/src/Exercise/Dictionaries/SaveableDictionary.cs
+++ part11/exercise_165/src/Exercise/Dictionaries/SaveableDictionary.cs
@@ -1,39 +1,117 @@
 namespace Exercise
 {
-  using System;
-  using System.IO;
-  using System.Collections.Generic;
-  public class SaveableDictionary
-  {
-    public SaveableDictionary()
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    public class SaveableDictionary
     {
-    }
+        private Dictionary<string, string> translateWords;
+        private string file;
+        public SaveableDictionary()
+        {
+            this.translateWords = new Dictionary<string, string>();
+        }
 
-    public SaveableDictionary(string file) : this()
-    {
-    }
+        public SaveableDictionary(string file) : this()
+        {
+            this.file = file;
+        }
 
-    public void Add(string word, string translation)
-    {
-    }
+        public void Add(string word, string translation)
+        {
+            if (!this.translateWords.ContainsKey(word))
+            {
+                this.translateWords.Add(word, translation);
+            }
 
-    public bool Load()
-    {
-      return false;
-    }
+        }
 
-    public bool Save()
-    {
-      return false;
-    }
+        public bool Load()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(this.file);
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(":");
+                    this.translateWords.Add(parts[0], parts[1]);
 
-    public string Translate(string word)
-    {
-      return null;
-    }
+                }
 
-    public void Delete(string word)
-    {
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+                return false;
+            }
+        }
+
+        public bool Save()
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(this.file);
+                foreach (KeyValuePair<string, string> kvp in this.translateWords)
+                {
+                    writer.WriteLine(kvp.Key + ":" + kvp.Value);
+                }
+                writer.Close();
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public string Translate(string word)
+        {
+            if (this.translateWords.ContainsKey(word))
+            {
+
+                return this.translateWords[word];
+            }
+            foreach (KeyValuePair<string, string> words in this.translateWords)
+            {
+                if (words.Value == word)
+                {
+                    return words.Key;
+                }
+
+            }
+
+            {
+                return null;
+            }
+        }
+
+        public void Delete(string word)
+        {
+            foreach (KeyValuePair<string, string> words in this.translateWords)
+            {
+                if (words.Key == word)
+                {
+                    this.translateWords.Remove(words.Key);
+                }
+                if (words.Value == word)
+                {
+                    this.translateWords.Remove(words.Key);
+                }
+            }
+        }
+        // not using this method but writing the foreach in Load() instead
+        /*public void NextLine(string[] words)
+        {
+          foreach (string line in words)
+          {
+            string [] parts = line.Split(":");
+            this.translateWords.Add(parts[0], parts[1]);
+
+          }
+        }*/
     }
-  }
 }
